7bee0cec54ecdc06544f036900db1d16eaed9cfe
coderapex <coder.apex@gmail.com>
2018-11-20 23:59:36 +0530

    The 'Hello World' of React created

---
+++ a01-intro/src/index.js
@@ -0,0 +1,11 @@
+// Importing react and reactODM library
+import React from "react";
+import ReactDOM from "react-dom";
+
+// Create a react component
+const App = () => {
+  return <div>Hi there!</div>;
+};
+
+// Show the component on the screen
+ReactDOM.render(<App />, document.querySelector("#root"));
